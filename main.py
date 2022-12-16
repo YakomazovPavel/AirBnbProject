22ea3556c64b23e220205fc9c1ea0a54709e1c15
михан <>
2022-12-17 00:45:50 +0300

    added map new

--- main.py
+++ main.py
@@ -11,30 +11,30 @@ df = pd.read_csv("AB_US_2020.csv").head(1000)
 df[['latitude', 'longitude']] = df[['latitude', 'longitude']].astype(float)
 print('')
 
-class LatLngPopup(MacroElement):
-    """
-    When one clicks on a Map that contains a LatLngPopup,
-    a popup is shown that displays the latitude and longitude of the pointer.
-    """
-    _template = Template(u"""
-            {% macro script(this, kwargs) %}
-                var {{this.get_name()}} = L.popup();
-                function latLngPop(e) {
-                    {{this.get_name()}}
-                        .setLatLng(e.latlng)
-                        .setContent("Latitude: " + e.latlng.lat.toFixed(4) +
-                                    "<br>Longitude: " + e.latlng.lng.toFixed(4))
-                        .openOn({{this._parent.get_name()}});
-                    parent.document.getElementById("id_lng").value = e.latlng.lng.toFixed(4); # add this
-                    parent.document.getElementById("id_lat").value = e.latlng.lat.toFixed(4); # add this
-                    }
-                {{this._parent.get_name()}}.on('click', latLngPop);
-            {% endmacro %}
-            """)  # noqa
-
-    def __init__(self):
-        super(LatLngPopup, self).__init__()
-        self._name = 'LatLngPopup'
+# class LatLngPopup(MacroElement):
+#     """
+#     When one clicks on a Map that contains a LatLngPopup,
+#     a popup is shown that displays the latitude and longitude of the pointer.
+#     """
+#     _template = Template(u"""
+#             {% macro script(this, kwargs) %}
+#                 var {{this.get_name()}} = L.popup();
+#                 function latLngPop(e) {
+#                     {{this.get_name()}}
+#                         .setLatLng(e.latlng)
+#                         .setContent("Latitude: " + e.latlng.lat.toFixed(4) +
+#                                     "<br>Longitude: " + e.latlng.lng.toFixed(4))
+#                         .openOn({{this._parent.get_name()}});
+#                     parent.document.getElementById("id_lng").value = e.latlng.lng.toFixed(4); # add this
+#                     parent.document.getElementById("id_lat").value = e.latlng.lat.toFixed(4); # add this
+#                     }
+#                 {{this._parent.get_name()}}.on('click', latLngPop);
+#             {% endmacro %}
+#             """)  # noqa
+#
+#     def __init__(self):
+#         super(LatLngPopup, self).__init__()
+#         self._name = 'LatLngPopup'
 
 # columnsNames = list(df.columns.values)
 # countOfNull = []
